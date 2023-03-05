468e023f018e405581a36084804767fe78062ec5
Buddha <buddhagautam231@gmail.com>
2023-03-05 10:06:05 +0545

    ASP.net Project initiated

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace chat_room_for_nft_owners;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
